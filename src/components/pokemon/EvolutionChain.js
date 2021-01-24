f1878579b690a6b43fdef2fbf80a0ba0775b837f
minhazur9 <minhazur9@gmail.com>
2021-01-23 19:44:05 -0500

    evolution chain renders

--- src/components/pokemon/EvolutionChain.js
+++ src/components/pokemon/EvolutionChain.js
@@ -5,34 +5,64 @@ import { getEvolutionChain } from '../../actions';
 const EvolutionChain = () => {
     const speciesInfo = useSelector(state => state.speciesInfo);
     const evolutionChain = useSelector(state => state.evolutionChain);
-    const evolutionData = [];
     const dispatch = useDispatch();
 
     // dispatch(getEvolutionChain(speciesInfo.evolution_chain.url))
-    const getEvolutionData = (data) => {
-            if(data) {
-                evolutionData.push(data.species.name)
-                    data.evolves_to.map((form) => {
-                       if(form.evolves_to) return getEvolutionData(form)
-                       return 0;
-                    })
-            }
-    }
+    // const getEvolutionData = (data,branch) => {
+    //         if(data) {
+    //             evolutionData.push({name:data.species.name,branch:branch})
+    //                 data.evolves_to.map((form) => {
+    //                     console.log(data.evolves_to.indexOf(form))
+    //                    return getEvolutionData(form,data.evolves_to.indexOf(form))
+    //                 })
+    //         }
+    // }
 
-    const renderChain = () => {
-        getEvolutionData(evolutionChain);
-        return evolutionData.map((stage,index) => {
+    // const renderChain = () => {
+    //     getEvolutionData(evolutionChain,0);
+    //     console.log(evolutionData)
+    //     return evolutionData.map((stage,index) => {
+    //         return (
+    //             <>
+    //             <li className="stage-image" key={index}>
+    //                     <img src={`https://img.pokemondb.net/artwork/${stage.name}.jpg`} alt={stage}/>
+    //                     <p>{stage.name.toUpperCase()}</p>
+    //             </li>
+    //             <div key={index + 10}>{evolutionData[index + 1] && <i key={index + 10} className="fa fa-long-arrow-right" aria-hidden="false"></i>}</div>
+    //             </>
+                
+    //         )
+    //     })
+    // }
+
+    const renderFirstStage = (stage) => {
+        if(stage) {
             return (
                 <>
-                <li className="stage-image" key={index}>
-                        <img src={`https://img.pokemondb.net/artwork/${stage}.jpg`} alt={stage}/>
-                        <p>{stage.toUpperCase()}</p>
+                <li className="stage-image">
+                        <img src={`https://img.pokemondb.net/artwork/${stage.species.name}.jpg`} alt={stage}/>
+                        <p>{stage.species.name.toUpperCase()}</p>
                 </li>
-                {evolutionData[index + 1] && <i key={index + 10} className="fa fa-long-arrow-right" aria-hidden="false"></i>}
-                </>
-                
-            )
-        })
+                <div>{stage.evolves_to.length > 0 && <i className="fa fa-long-arrow-right" aria-hidden="false"></i>}</div>
+                </>              
+            );
+        }
+    }
+
+    const renderChain = (data) => {
+        if(data) {
+        return data.evolves_to.map((stage,index) => {
+            return (
+                    <>
+                    <li className="stage-image" key={index}>
+                            <img src={`https://img.pokemondb.net/artwork/${stage.species.name}.jpg`} alt={stage}/>
+                            <p>{stage.species.name.toUpperCase()}</p>
+                    </li>
+                    <div key={index + 10}>{stage.evolves_to.length > 0 && <i key={index + 10} className="fa fa-long-arrow-right" aria-hidden="false"></i>}</div>
+                    {stage.evolves_to.length > 0 && renderChain(stage)}
+                    </>              
+            );
+        })}
     }
 
     useEffect(() => {
@@ -44,7 +74,8 @@ const EvolutionChain = () => {
         <>
             <p className="evolution-chain-header">Evolution Chain</p>
             <ul className="evolution-line">
-                {renderChain()}
+                {renderFirstStage(evolutionChain)}
+                {renderChain(evolutionChain)}
             </ul>
         </>
     )
