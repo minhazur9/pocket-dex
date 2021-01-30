bc2c43ab9be05ada9c400daccfc4e46a30c401da
minhazur9 <minhazur9@gmail.com>
2021-01-29 21:08:30 -0500

    created a signup form

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -4,6 +4,17 @@ const SignUp = () => {
     return (
         <div className="signup">
             <h3>SIGN UP! CREATE YOUR BEST TEAMS!</h3>
+            <form className="signup-form">
+                <label htmlFor="username">Username</label>
+                <input className='signup-input' type="text" name='username' />
+                <label htmlFor="email">Email</label>
+                <input className='signup-input' type="text" name='email' />
+                <label htmlFor="password">Password</label>
+                <input className='signup-input' type="text" name='password' />
+                <label htmlFor="confirm">Confirm Password</label>
+                <input className='signup-input' type="text" name='confirm' />
+                <button className="waves-effect waves-light btn-large signup-submit">Signup</button>
+            </form>
         </div>
     )
 }
