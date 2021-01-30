3624d4fca431a0775a17443accdd27892e764753
minhazur9 <minhazur9@gmail.com>
2021-01-29 19:55:52 -0500

    passwords are now hashed when new users are created

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -12,7 +12,7 @@ const {
     GraphQLSchema,
     GraphQLList,
     GraphQLNonNull,
-    GraphQLString, 
+    GraphQLString,
     GraphQLID,
     GraphQLInt
 } = graphql;
@@ -25,45 +25,45 @@ const RootQuery = new GraphQLObjectType({
         // Query for a user
         user: {
             type: UserType,
-            args: {id: {type: new GraphQLNonNull(GraphQLID)}},
-            resolve(parent,args) {
+            args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+            resolve(parent, args) {
                 return db.User.findById(args.id)
             }
         },
         // Query for a team
         team: {
             type: TeamType,
-            args: {id: {type: new GraphQLNonNull(GraphQLID)}},
-            resolve(parent,args) {
+            args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+            resolve(parent, args) {
                 return db.Team.findById(args.id)
             }
         },
         // Query for a pokemon
         pokemon: {
             type: PokemonType,
-            args: {id: {type: new GraphQLNonNull(GraphQLID)}},
-            resolve(parent,args) {
+            args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+            resolve(parent, args) {
                 return db.Pokemon.findById(args.id)
             }
         },
         // Query for all users
         allUsers: {
             type: new GraphQLList(UserType),
-            resolve(parent,args) {
+            resolve(parent, args) {
                 return db.User.find({})
             }
         },
         // Query for all teams
         allTeams: {
             type: new GraphQLList(TeamType),
-            resolve(parent,args) {
+            resolve(parent, args) {
                 return db.Team.find({})
             }
         },
         // Query for all pokemon
         allPokemon: {
             type: new GraphQLList(PokemonType),
-            resolve(parent,args) {
+            resolve(parent, args) {
                 return db.Pokemon.find({})
             }
         }
@@ -78,25 +78,36 @@ const Mutation = new GraphQLObjectType({
         addUser: {
             type: UserType,
             args: {
-                username: {type: new GraphQLNonNull(GraphQLString)},
-                email: {type: new GraphQLNonNull(GraphQLString)},
-                password: {type: new GraphQLNonNull(GraphQLString)},
+                username: { type: new GraphQLNonNull(GraphQLString) },
+                email: { type: new GraphQLNonNull(GraphQLString) },
+                password: { type: new GraphQLNonNull(GraphQLString) },
             },
-            resolve(parent,args) {
-
-                return db.User.create({
-                    username: args.username,
-                })
+            resolve(parent, args) {
+                db.User.findOne({ $or: [{ email: args.email }, { username: args.username }] })
+                    .then((foundUser) => {
+                        if (foundUser) {
+                            console.log("Username/Email Taken")
+                            return null;
+                        }
+                            
+                        return bycrypt.genSalt(10)
+                            .then((salt) => bycrypt.hash(args.password, salt))
+                            .then((hashedPassword) => db.User.create({
+                                username: args.username,
+                                email: args.email,
+                                password: hashedPassword
+                            }))
+                    })
             }
         },
         // Adds new team to a user
         addTeam: {
             type: TeamType,
             args: {
-                name:{type: new GraphQLNonNull(GraphQLString)},
-                userId:{type: new GraphQLNonNull(GraphQLID)}
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                userId: { type: new GraphQLNonNull(GraphQLID) }
             },
-            resolve(parent,args) {
+            resolve(parent, args) {
                 return db.Team.create({
                     name: args.name,
                     userId: args.userId,
@@ -107,9 +118,9 @@ const Mutation = new GraphQLObjectType({
         addPokemon: {
             type: PokemonType,
             args: {
-                name:{type: new GraphQLNonNull(GraphQLString)},
-                level:{type: new GraphQLNonNull(GraphQLInt)},
-                teamId: {type: new GraphQLNonNull(GraphQLID)},
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                level: { type: new GraphQLNonNull(GraphQLInt) },
+                teamId: { type: new GraphQLNonNull(GraphQLID) },
             },
             resolve(parent, args) {
                 return db.Pokemon.create({
@@ -123,82 +134,82 @@ const Mutation = new GraphQLObjectType({
         deleteUser: {
             type: UserType,
             args: {
-                id: {type: new GraphQLNonNull(GraphQLID)}
+                id: { type: new GraphQLNonNull(GraphQLID) }
             },
-            resolve(parent,args) {
+            resolve(parent, args) {
                 return db.User.findByIdAndDelete(args.id)
-                .then( async (foundUser) => {
-                    const foundTeams = await db.Team.find({userId:foundUser._id});
-                    await db.Team.deleteMany({userId:foundUser._id});
-                    return foundTeams;
-                })
-                .then((foundTeams) => {
-                    const teamIds = foundTeams.map(({_id}) => _id)
-                    return db.Pokemon.deleteMany({ teamId:{$in: teamIds}})
-                })
+                    .then(async (foundUser) => {
+                        const foundTeams = await db.Team.find({ userId: foundUser._id });
+                        await db.Team.deleteMany({ userId: foundUser._id });
+                        return foundTeams;
+                    })
+                    .then((foundTeams) => {
+                        const teamIds = foundTeams.map(({ _id }) => _id)
+                        return db.Pokemon.deleteMany({ teamId: { $in: teamIds } })
+                    })
             }
         },
         // Deletes a team from a user
         deleteTeam: {
             type: TeamType,
             args: {
-                id: {type:new GraphQLNonNull(GraphQLID)}
+                id: { type: new GraphQLNonNull(GraphQLID) }
             },
-            resolve(parent,args) {
+            resolve(parent, args) {
                 return db.Team.findByIdAndDelete(args.id)
-                .then((foundTeam) => db.Pokemon.deleteMany({teamId:foundTeam._id}))
+                    .then((foundTeam) => db.Pokemon.deleteMany({ teamId: foundTeam._id }))
             }
         },
         // Deletes a pokemon from a team
         deletePokemon: {
             type: PokemonType,
             args: {
-                id: {type: new GraphQLNonNull(GraphQLID)}
+                id: { type: new GraphQLNonNull(GraphQLID) }
             },
-            resolve(parent,args) {
-                return db.Pokemon.findOneAndDelete({_id:args.id})
+            resolve(parent, args) {
+                return db.Pokemon.findOneAndDelete({ _id: args.id })
             }
         },
         // Edits user information
         editUser: {
             type: UserType,
             args: {
-                id: {type: new GraphQLNonNull(GraphQLID)},
-                username: {type: new GraphQLNonNull(GraphQLString)}
+                id: { type: new GraphQLNonNull(GraphQLID) },
+                username: { type: new GraphQLNonNull(GraphQLString) }
             },
-            resolve(parent,args) {
+            resolve(parent, args) {
                 return db.User.findByIdAndUpdate(
                     args.id,
-                    {$set: {username: args.username}},
-                    {new: true})
+                    { $set: { username: args.username } },
+                    { new: true })
             }
         },
         // Edits team information
         editTeam: {
             type: TeamType,
             args: {
-                id: {type: new GraphQLNonNull(GraphQLID)},
-                name: {type: new GraphQLNonNull(GraphQLString)}
+                id: { type: new GraphQLNonNull(GraphQLID) },
+                name: { type: new GraphQLNonNull(GraphQLString) }
             },
-            resolve(parent,args) {
+            resolve(parent, args) {
                 return db.Team.findByIdAndUpdate(
                     args.id,
-                    {$set: {name: args.name}},
-                    {new: true})
+                    { $set: { name: args.name } },
+                    { new: true })
             }
         },
         // Edits pokemon information
         editPokemon: {
             type: PokemonType,
             args: {
-                id: {type: new GraphQLNonNull(GraphQLID)},
-                name: {type: new GraphQLNonNull(GraphQLString)}
+                id: { type: new GraphQLNonNull(GraphQLID) },
+                name: { type: new GraphQLNonNull(GraphQLString) }
             },
-            resolve(parent,args) {
+            resolve(parent, args) {
                 return db.Pokemon.findByIdAndUpdate(
                     args.id,
-                    {$set: {name: args.name}},
-                    {new: true})
+                    { $set: { name: args.name } },
+                    { new: true })
             }
         }
     }
