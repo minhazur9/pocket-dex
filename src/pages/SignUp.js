ac10e875e07fc893ecee075be5b94c7fae612340
minhazur9 <minhazur9@gmail.com>
2021-01-29 21:15:40 -0500

    sign up forms now track state

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignUp = () => {
+
+    const [username,setUsername] = useState("");
+    const [email,setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirm, setConfirm] = useState("");
+
+
     return (
         <div className="signup">
             <h3>SIGN UP! CREATE YOUR BEST TEAMS!</h3>
             <form className="signup-form">
                 <label htmlFor="username">Username</label>
-                <input className='signup-input' type="text" name='username' />
+                <input className='signup-input' type="text" name='username' onChange={(e) => setUsername(e.target.value)}/>
                 <label htmlFor="email">Email</label>
-                <input className='signup-input' type="text" name='email' />
+                <input className='signup-input' type="email" name='email' onChange={(e) => setEmail(e.target.value)} />
                 <label htmlFor="password">Password</label>
-                <input className='signup-input' type="text" name='password' />
+                <input className='signup-input' type="password" name='password' onChange={(e) => setPassword(e.target.value)} />
                 <label htmlFor="confirm">Confirm Password</label>
-                <input className='signup-input' type="text" name='confirm' />
+                <input className='signup-input' type="password" name='confirm'  onChange={(e) => setConfirm(e.target.value)}/>
                 <button className="waves-effect waves-light btn-large signup-submit">Signup</button>
             </form>
         </div>
