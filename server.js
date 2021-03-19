1362a1b2c5ee9b33688faab29ba5d14a3d937614
minhazur9 <minhazur9@gmail.com>
2021-03-19 03:12:22 -0400

    deploy again

--- server.js
+++ server.js
@@ -24,17 +24,17 @@ app.use(session({
     saveUninitialized: true
 }))
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
 
-app.use(express.static('build'))
-
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build','index.html'));
+    res.sendFile(path.join(__dirname, 'build','index.html'));
 })
 
 app.listen(PORT, () => {
